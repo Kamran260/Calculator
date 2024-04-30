7a5435767be6cb097eeb1d13a7938251527fdf80
Kamran260 <mk9338880@gmail.com>
2024-04-30 11:58:39 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let calculator = await inquirer.prompt([
+    {
+        type: 'number',
+        name: 'num1',
+        message: 'enter a number'
+    },
+    {
+        type: 'number',
+        name: 'num2',
+        message: 'enter a 2nd number'
+    },
+    {
+        type: 'list',
+        name: 'operator',
+        message: 'Select Operator',
+        choices: ['Add', 'Subtract', 'Divide', 'Multiply']
+    }
+]);
+let result;
+switch (calculator.operator) {
+    case 'Add':
+        result = calculator.num1 + calculator.num2;
+        console.log('Addition of given numbers is ' + result);
+        break;
+    case 'Subtract':
+        result = calculator.num1 - calculator.num2;
+        console.log('Subtraction of given numbers is ' + result);
+        break;
+    case 'Divide':
+        result = calculator.num1 / calculator.num2;
+        console.log('Divide of given numbers is ' + result);
+        break;
+    case 'Multiply':
+        result = calculator.num1 * calculator.num2;
+        console.log('Multiple of given numbers is ' + result);
+        break;
+}
